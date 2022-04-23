0dfae34fa259191a6522b3b959af57ce105d4b84
RehamMuhammad <reham.muhammad.helmy@gmail.com>
2022-04-23 20:43:39 +0200

    Basics of middleware & uses

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,7 +1,16 @@
 const routes = (app) => {
     app.route("/contacts")
-        .get((req, res) =>
-            res.send("GET Request Successfully!"))
+        .get((req, res, next) => {
+
+            //middleware
+            console.log(`Request from: ${req.originalUrl}`)
+            console.log(`Request type: ${req.method}`)
+            next()
+        }, (req, res, next) => {
+            res.send("GET Request Successfully!")
+
+        }
+        )
 
         .post((req, res) =>
             res.send("POST Request Successfully!"))
@@ -16,3 +25,12 @@ const routes = (app) => {
 
 
 export default routes;
+
+//Middleware functions are functions that have access to the request object (req), the response object (res),
+//and the next function in the application’s request-response cycle.
+//The next function is a function in the Express router which, when invoked, executes the middleware succeeding the current middleware.
+
+// Middleware functions can perform the following tasks:
+// Execute any code.
+// Make changes to the request and the response objects.
+// End the request-response cycle
