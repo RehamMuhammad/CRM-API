b957f399ee4eee9a4a31a37d9b4cd2d9f5a37403
RehamMuhammad <reham.muhammad.helmy@gmail.com>
2022-04-23 19:58:32 +0200

    Initial Server Files & Folders Structure

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 4000;
 
 app.get('/', (req, res) => {
-    res.send(`Node & express server is running on port ${PORT}`)
+    res.send(`Node & express server are running on port ${PORT}`)
 })
 
 app.listen(PORT, () => {
