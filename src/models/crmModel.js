1312d49759a778190261620288216e214b5228e3
RehamMuhammad <reham.muhammad.helmy@gmail.com>
2022-04-24 15:17:10 +0200

    Schema Setup

---
+++ src/models/crmModel.js
@@ -0,0 +1,28 @@
+//Schema defines the rules that your db can accept.
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+export const ContactShema = new Schema({
+    firstName: {
+        type: String,
+        required: "Please Enter your first name",
+    },
+    lastName: {
+        type: String,
+        required: "Please Enter your last name",
+    },
+    email: {
+        type: String
+    },
+    company: {
+        type: String
+    },
+    phoneNumber: {
+        type: Number
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now()
+    }
+})
